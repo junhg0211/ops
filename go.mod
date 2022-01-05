d318611bb59998d30d0a09da59bbdfe11a13af45
Sch <junhg0211@gmail.com>
2022-01-05 16:57:40 +0900

    Using net/http instead of gin-gonic/gin

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module ops
 go 1.16
 
 require (
-	github.com/gin-contrib/multitemplate v0.0.0-20220102045447-8a3ac507ec70 // indirect
-	github.com/gin-gonic/gin v1.7.7 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/noirbizarre/gonja v0.0.0-20200629003239-4d051fd0be61 // indirect
 )
--- main.go
+++ main.go
@@ -2,48 +2,119 @@ package main
 
 import (
 	"fmt"
-	"github.com/gin-contrib/multitemplate"
-	"github.com/gin-gonic/gin"
+	"github.com/gorilla/mux"
+	"github.com/noirbizarre/gonja"
 	"net/http"
 )
 
-func getRenderer() multitemplate.Renderer {
-	r := multitemplate.NewRenderer()
-	r.AddFromFiles("index", "templates/base.html", "templates/index.html")
-	r.AddFromFiles("profile", "templates/base.html", "templates/profile.html")
-	r.AddFromFiles("upload", "templates/base.html", "templates/upload.html")
-	return r
+func index(w http.ResponseWriter, _ *http.Request) {
+	tpl, err := gonja.FromFile("templates/index.html")
+	if err != nil {
+		fmt.Println("importing index templates:", err)
+		return
+	}
+
+	out, err := tpl.Execute(nil)
+	if err != nil {
+		fmt.Println("executing index templates:", err)
+		return
+	}
+
+	_, err = w.Write([]byte(out))
+	if err != nil {
+		fmt.Println("writing index:", err)
+		return
+	}
 }
 
-func index(c *gin.Context) {
-	c.HTML(http.StatusOK, "index", nil)
-}
+func profile(w http.ResponseWriter, req *http.Request) {
+	tpl, err := gonja.FromFile("templates/profile.html")
+	if err != nil {
+		fmt.Println("importing profile templates:", err)
+		return
+	}
 
-func profile(c *gin.Context) {
-	email := c.Param("email")
-	c.HTML(http.StatusOK, "profile", gin.H{
-		"username": "Sch",
+	param := mux.Vars(req)
+	var email = param["email"]
+	var isUser = email == "junhg0211@gmail.com"
+	var username string
+
+	if isUser {
+		username = "스치"
+	} else {
+		username = "정보 없음"
+	}
+	out, err := tpl.Execute(gonja.Context{
 		"email":    email,
+		"is_user":  isUser,
+		"username": username,
 	})
+
+	if err != nil {
+		fmt.Println("executing profile templates:", err)
+		return
+	}
+	_, err = w.Write([]byte(out))
+	if err != nil {
+		fmt.Println("writing profile:", err)
+		return
+	}
 }
 
-func upload(c *gin.Context) {
-	c.HTML(http.StatusOK, "upload", nil)
+func upload(w http.ResponseWriter, _ *http.Request) {
+	tpl, err := gonja.FromFile("templates/upload.html")
+	if err != nil {
+		fmt.Println("importing upload templates:", err)
+		return
+	}
+
+	out, err := tpl.Execute(nil)
+
+	if err != nil {
+		fmt.Println("executing upload templates:", err)
+		return
+	}
+	_, err = w.Write([]byte(out))
+	if err != nil {
+		fmt.Println("writing upload:", err)
+		return
+	}
+}
+
+func problem(w http.ResponseWriter, req *http.Request) {
+	tpl, err := gonja.FromFile("templates/problem.html")
+	if err != nil {
+		fmt.Println("importing problem templates:", err)
+		return
+	}
+
+	param := mux.Vars(req)
+	out, err := tpl.Execute(gonja.Context{
+		"problem_code": param["problem_code"],
+	})
+
+	if err != nil {
+		fmt.Println("executing problem templates:", err)
+		return
+	}
+	_, err = w.Write([]byte(out))
+	if err != nil {
+		fmt.Println("writing problem:", err)
+		return
+	}
 }
 
 func main() {
-	router := gin.Default()
-	router.Static("/assets", "assets")
-	router.HTMLRender = getRenderer()
+	router := mux.NewRouter()
+	router.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+	router.HandleFunc("/", index)
+	router.HandleFunc("/index", index)
+	router.HandleFunc("/profile/{email}", profile)
+	router.HandleFunc("/upload", upload)
+	router.HandleFunc("/problem/{problem_code:[A-Z]+\\d+}", problem)
 
-	router.GET("/", index)
-	router.GET("/index", index)
-	router.GET("/profile/:email", profile)
-	router.GET("/upload", upload)
-
-	err := router.Run(":80")
+	err := http.ListenAndServe(":80", router)
 	if err != nil {
-		fmt.Println("running router:", err)
-		return
+		fmt.Println("listening and serving:", err)
 	}
 }
